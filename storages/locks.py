d3c440f6bdd45471ed24166dc915e0825e5c1f2b
Donald Stufft <donald.stufft@gmail.com>
2012-07-18 18:55:15 -0400

    Use unicode literals

--- storages/locks.py
+++ storages/locks.py
@@ -15,6 +15,7 @@ Example Usage::
 """
 from __future__ import absolute_import
 from __future__ import division
+from __future__ import unicode_literals
 
 
 __all__ = ("LOCK_EX", "LOCK_SH", "LOCK_NB", "lock", "unlock")
