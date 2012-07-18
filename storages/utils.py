c42574f08768f62de856aad28e1ae315290e788c
Donald Stufft <donald.stufft@gmail.com>
2012-07-18 19:02:31 -0400

    Backport the 2.7 import_module

--- storages/utils.py
+++ storages/utils.py
@@ -5,6 +5,7 @@ from __future__ import unicode_literals
 
 import os
 import re
+import sys
 import urllib
 
 from . import locks
@@ -24,7 +25,7 @@ except ImportError:
             os.chmod(dst, mode)
 
 
-__all__ = ("abspath", "file_move_safe", "filepath_to_uri", "get_valid_filename", "safe_join")
+__all__ = ("abspath", "file_move_safe", "filepath_to_uri", "get_valid_filename", "import_module", "safe_join")
 
 
 def get_valid_filename(s):
@@ -168,3 +169,44 @@ def filepath_to_uri(path):
     # I know about `os.sep` and `os.altsep` but I want to leave
     # some flexibility for hardcoding separators.
     return urllib.quote(path.replace("\\", "/"), safe=b"/~!*()'")
+
+
+def _resolve_name(name, package, level):
+    """
+    Return the absolute name of the module to be imported.
+    """
+    if not hasattr(package, "rindex"):
+        raise ValueError("'package' not set to a string")
+
+    dot = len(package)
+    for x in xrange(level, 1, -1):
+        try:
+            dot = package.rindex('.', 0, dot)
+        except ValueError:
+            raise ValueError("attempted relative import beyond top-level "
+                              "package")
+    return "%s.%s" % (package[:dot], name)
+
+
+def import_module(name, package=None):
+    """
+    Import a module.
+
+    The 'package' argument is required when performing a relative import. It
+    specifies the package to use as the anchor point from which to resolve the
+    relative import to an absolute import.
+
+    """
+    if name.startswith("."):
+        if not package:
+            raise TypeError("relative imports require the 'package' argument")
+        level = 0
+        for character in name:
+            if character != ".":
+                break
+            level += 1
+        name = _resolve_name(name[level:], package, level)
+
+    __import__(name)
+
+    return sys.modules[name]
