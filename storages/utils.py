29fee19b990dbb855238ad242f4c81cd107fc444
Donald Stufft <donald.stufft@gmail.com>
2012-07-18 19:03:25 -0400

    Remove safe move function as we no longer need it

--- storages/utils.py
+++ storages/utils.py
@@ -8,24 +8,8 @@ import re
 import sys
 import urllib
 
-from . import locks
 
-try:
-    from shutil import copystat
-except ImportError:
-    import stat
-
-    def copystat(src, dst):
-        """Copy all stat info (mode bits, atime and mtime) from src to dst"""
-        st = os.stat(src)
-        mode = stat.S_IMODE(st.st_mode)
-        if hasattr(os, 'utime'):
-            os.utime(dst, (st.st_atime, st.st_mtime))
-        if hasattr(os, 'chmod'):
-            os.chmod(dst, mode)
-
-
-__all__ = ("abspath", "file_move_safe", "filepath_to_uri", "get_valid_filename", "import_module", "safe_join")
+__all__ = ("abspath", "filepath_to_uri", "get_valid_filename", "import_module", "safe_join")
 
 
 def get_valid_filename(s):
@@ -59,69 +43,6 @@ else:
         if not os.path.isabs(path):
             path = os.path.join(os.getcwdu(), path)
         return os.path.normpath(path)
-
-
-def _samefile(src, dst):
-    # Macintosh, Unix.
-    if hasattr(os.path, "samefile"):
-        try:
-            return os.path.samefile(src, dst)
-        except OSError:
-            return False
-
-    # All other platforms: check for same pathname.
-    return (os.path.normcase(os.path.abspath(src)) ==
-            os.path.normcase(os.path.abspath(dst)))
-
-
-def file_move_safe(old_file_name, new_file_name, chunk_size=1024 * 64, allow_overwrite=False):
-    """
-    Moves a file from one location to another in the safest way possible.
-
-    First, tries ``os.rename``, which is simple but will break across filesystems.
-    If that fails, streams manually from one file to another in pure Python.
-
-    If the destination file exists and ``allow_overwrite`` is ``False``, this
-    function will throw an ``IOError``.
-    """
-
-    # There's no reason to move if we don't have to.
-    if _samefile(old_file_name, new_file_name):
-        return
-
-    try:
-        os.rename(old_file_name, new_file_name)
-        return
-    except OSError:
-        # This will happen with os.rename if moving to another filesystem
-        # or when moving opened files on certain operating systems
-        pass
-
-    # first open the old file, so that it won't go away
-    with open(old_file_name, 'rb') as old_file:
-        # now open the new file, not forgetting allow_overwrite
-        fd = os.open(new_file_name, os.O_WRONLY | os.O_CREAT | getattr(os, 'O_BINARY', 0) |
-                                    (not allow_overwrite and os.O_EXCL or 0))
-        try:
-            locks.lock(fd, locks.LOCK_EX)
-            current_chunk = None
-            while current_chunk != '':
-                current_chunk = old_file.read(chunk_size)
-                os.write(fd, current_chunk)
-        finally:
-            locks.unlock(fd)
-            os.close(fd)
-    copystat(old_file_name, new_file_name)
-
-    try:
-        os.remove(old_file_name)
-    except OSError as e:
-        # Certain operating systems (Cygwin and Windows)
-        # fail when deleting opened files, ignore it.  (For the
-        # systems where this happens, temporary files will be auto-deleted
-        # on close anyway.)
-        if getattr(e, 'winerror', 0) != 32 and getattr(e, 'errno', 0) != 13:
-            raise
 
 
 def safe_join(base, *paths):
